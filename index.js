1342ac8e4dae82a9e329e0488c7c6026f2757e0b
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-24 00:53:15 -0700

    delete files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { request } = require("@octokit/request");
 const wtd = require("what-the-diff");
-const fetch = require('node-fetch');
 var fs = require("fs");
 
 const ev = JSON.parse(
@@ -27,22 +26,6 @@ const [owner, repo] = owner_repo.split("/");
 console.log("owner: " + owner)
 
 console.log("repo: " + repo)
-
-async function files() {
-    // https://github.com/rvantonder/silly-test-repo/pull/2.diff
-
-  const response = await fetch(`https://github.com/${owner}/${repo}/pull/${prNum}.diff`);
-  const body = await response.text();
-
-  console.log('diff: ' + body);
-
-  const diffs = wtd.parse(body);
-
-  for (var i = 0; i < diffs.length; i++) {
-    console.log("iterating diff file" + diffs[i].newPath)
-    var hunks = diffs[i].hunks;
-  }
-}
 
 async function run(path, suggestion, rangeStart, rangeEnd) {
   const open = "```suggestion";
@@ -75,8 +58,6 @@ async function run(path, suggestion, rangeStart, rangeEnd) {
     }
   );
 }
-
-files();
 
 fs.readFile("p.patch", "utf8", function(err, contents) {
   const diffs = wtd.parse(contents);
