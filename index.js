93fe2826101c9207379d1e424fd4f9e3561105e1
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-24 01:12:31 -0700

    lol semicolons

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const wtd = require("what-the-diff");
 var fs = require("fs");
 
 const ev = JSON.parse(
-  fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-)
-const prNum = ev.pull_request.number
+  fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
+);
+const prNum = ev.pull_request.number;
 
-console.log('pr ' + prNum)
+console.log('pr ' + prNum);
 
 // https://github.community/t/github-sha-isnt-the-value-expected/17903/2
 //const sha = process.env.GITHUB_SHA
-const sha = process.env.PR_HEAD_SHA
+const sha = process.env.PR_HEAD_SHA;
 
-console.log('sha ' + sha)
+console.log('sha ' + sha);
 
-const token = process.env.GITHUB_TOKEN
+const token = process.env.GITHUB_TOKEN;
 
-console.log('token ' + token)
+console.log('token ' + token);
 
-owner_repo = process.env.GITHUB_REPOSITORY
+owner_repo = process.env.GITHUB_REPOSITORY;
 
 const [owner, repo] = owner_repo.split("/");
 
-console.log("owner: " + owner)
+console.log("owner: " + owner);
 
-console.log("repo: " + repo)
+console.log("repo: " + repo);
 
 async function run(path, suggestion, rangeStart, rangeEnd) {
   const open = "```suggestion";
   const close = "```";
   const body = `${open}` + "\n" + suggestion + `${close}`;
 
-  path = path.slice(path.indexOf("/")+1) // strip b/
-  console.log("requesting with prNum: " + prNum)
-  console.log("with sha: " + sha)
-  console.log("path: " + path)
+  path = path.slice(path.indexOf("/")+1); // strip b/
+  console.log("requesting with prNum: " + prNum);
+  console.log("with sha: " + sha);
+  console.log("path: " + path);
   const result = await request(
     "POST /repos/:owner/:repo/pulls/:pull_number/comments",
     {
@@ -64,7 +64,7 @@ fs.readFile("p.patch", "utf8", function(err, contents) {
   console.log(diffs);
   console.log("diffs is\n" + JSON.stringify(diffs));
   for (var i = 0; i < diffs.length; i++) {
-    console.log("iterating diff " + 1)
+    console.log("iterating diff " + 1);
     var hunks = diffs[i].hunks;
     for (var j = 0; j < hunks.length; j++) {
       console.log("iterating hunk " + j + "\n" + JSON.stringify(hunks[j]));
@@ -74,18 +74,18 @@ fs.readFile("p.patch", "utf8", function(err, contents) {
       for (var k = 0; k < lines.length; k++) {
         var line = lines[k];
         if (!line.startsWith("-")) {
-          line = line.substr(1) // remove '+' or space
-          suggest += line + "\n"
+          line = line.substr(1); // remove '+' or space
+          suggest += line + "\n";
         }
       }
-      rangeStart = hunks[j].oldStartLine
-      rangeEnd = rangeStart + hunks[j].oldLineCount - 1
-      console.log("suggest at: " + rangeStart + " to " + rangeEnd)
-      console.log("=====")
-      console.log(suggest)
-      console.log("-----")
+      rangeStart = hunks[j].oldStartLine;
+      rangeEnd = rangeStart + hunks[j].oldLineCount - 1;
+      console.log("suggest at: " + rangeStart + " to " + rangeEnd);
+      console.log("=====");
+      console.log(suggest);
+      console.log("-----");
       run(diffs[i].newPath, suggest, rangeStart, rangeEnd);
-      console.log("sent")
+      console.log("sent");
     }
   }
 });
